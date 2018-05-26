5211f8f0ff23f8440f1a40b47d0d7bfce62df34e
Allex Vissoci <allex.vissoci@gmail.com>
2018-05-25 21:00:02 -0300

    creatind record model

--- record/models.py
+++ record/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+class Record(models.Model):
+    id = models.AutoField(primary_key=True)
+    type = models.CharField(max_length=5)
+    timestamp = models.DateTimeField()
+    call_id = models.CharField(max_length=200)
+    source = models.CharField(
+                                max_length=11, blank=True, null=True,
+                                validators=[MinLengthValidator(10)])
+    destination = models.CharField(
+                                max_length=11, blank=True, null=True,
+                                validators=[MinLengthValidator(10)])
+
+    class Meta:
+        unique_together = (("type", "call_id"))
+
+    def __str__(self):
+        return "{} {} {}".format(self.id, self.call_id, self.timestamp)
