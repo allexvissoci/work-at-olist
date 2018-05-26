9eae45b387278944b6a214f3b5ab77901e7204d0
Allex Vissoci <allex.vissoci@gmail.com>
2018-05-25 23:02:44 -0300

    create bill model

--- bill/models.py
+++ bill/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Bill(models.Model):
+    call_start_record = models.ForeignKey('record.Record',
+                                          on_delete=models.CASCADE,
+                                          related_name='call_start_record')
+    call_end_record = models.ForeignKey('record.Record',
+                                        on_delete=models.CASCADE,
+                                        related_name='call_end_record')
+    call_duration = models.DurationField()
+    call_price = models.DecimalField(max_digits=5, decimal_places=2, null=True)
+
+    def __str__(self):
+        return self.call_start_record.source
